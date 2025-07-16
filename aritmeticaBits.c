a47493bdb23c17515b4fd8ac4d567e3453921721
Tomas Vecchi <204607402+tvecchiUTN@users.noreply.github.com>
2025-07-16 08:56:32 -0300

    Creacion de funcion 1.a
    Se creo la funcion para el ejercicio 1.a

--- aritmeticaBits.c
+++ aritmeticaBits.c
@@ -1,2 +1,8 @@
 #include <stdio.h>
+#include <stdint.h>
 #include "aritmeticaBits.h"
+
+int nibMenS(uint32_t val, int n)
+{
+
+}
--- aritmeticaBits.h
+++ aritmeticaBits.h
@@ -1,5 +1,6 @@
 #ifndef __ARITMETICABITS_H
 #define __ARITMETICABITS_H
 
+int nibMenS(uint32_t val, int n);
 
 #endif
